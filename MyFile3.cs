eff4e52f99aa530172640fd1ebbc5281332f1070
satyaCsgithub <satya_inet@yahoo.com>
2021-04-29 20:36:38 +0530

    first commit

---
+++ MyFile3.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class OfcDetail
+    {
+        public int PostId { get; set; }
+        public string Location { get; set; }
+    }
+}
